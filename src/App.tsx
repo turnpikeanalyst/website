7b98a4ce4ea1ae2384eba1623e940df07ca83cd0
Aditya192003 <adityakr192003@gmail.com>
2025-02-13 23:07:18 +0530

    new Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
+import Navbar from './components/Navbars'
 import Routing from './Routing'
 
 function App() {
